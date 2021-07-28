8660e5d527078be36243dc454782645c365af92d
Yordan Krushkov <yordankrushkov@gmail.com>
2021-07-28 20:38:30 +0100

    /backend

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const products = require('./Routes/products');
+const config = require('./Config/config');
 const users = require('./Routes/users');
 const db = require('./Config/database');
-let port = process.env.PORT || 4500
 
 require('./Config/express')(app);
 app.use('/phones', products)
 app.use('/user', users)
-app.listen(port, (err) => {
+app.listen(process.env.PORT||config.port, (err) => {
 
     if (err) {
         console.log("Problem with the connection:", err);
         return;
     }
     db()
-    console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${config.port}`);
 })
